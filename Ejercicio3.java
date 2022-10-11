3f827b73e25218c2ca8d318e4279594c5bf25faf
Adrian <chopon007@gmail.com>
2022-10-11 16:26:10 +0200

    Ejercicio3 changed

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,14 +1,32 @@
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         1.   Salida por pantalla                                                 *
+    *                                                                                  *
+    *         3.   Escribe un programa que muestre por pantalla 10 palabras            *
+    *              en inglés junto a su correspondiente traducción al                  *
+    *              castellano. Las palabras debes estar distribuidas en dos            *
+    *              columnas y alineadas a la izquierda.                                *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 public class Ejercicio3 {
     public static void main(String[] args) {
-        System.out.printf("%-10s    %-10s\n", "computer","ordenador");
-        System.out.printf("%-10s    %-10s\n", "student","alumno\\a");
-        System.out.printf("%-10s    %-10s\n", "cat","gato");
-        System.out.printf("%-10s    %-10s\n", "penguin","pingüino");
-        System.out.printf("%-10s    %-10s\n", "machine","máquina");
-        System.out.printf("%-10s    %-10s\n", "nature","naturaleza");
-        System.out.printf("%-10s    %-10s\n", "light","luz");
-        System.out.printf("%-10s    %-10s\n", "green","verde");
-        System.out.printf("%-10s    %-10s\n", "book","libro");
-        System.out.printf("%-10s    %-10s\n", "pyramid","pirámide");
+        //Outputs
+        System.out.printf("%-10s %s\n", "computer", "ordenador");
+        System.out.printf("%-10s %s\n", "student", "alumno\\a");
+        System.out.printf("%-10s %s\n", "cat", "gato");
+        System.out.printf("%-10s %s\n", "penguin", "pingüino");
+        System.out.printf("%-10s %s\n", "machine", "máquina");
+        System.out.printf("%-10s %s\n", "nature", "naturaleza");
+        System.out.printf("%-10s %s\n", "light", "luz");
+        System.out.printf("%-10s %s\n", "green", "verde");
+        System.out.printf("%-10s %s\n", "book", "libro");
+        System.out.printf("%-10s %s\n", "pyramid", "pirámide");
     }
 }
