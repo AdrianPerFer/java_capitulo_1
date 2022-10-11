4e5f6e1c151f85606759e712d2f4f33139558963
Adrian <chopon007@gmail.com>
2022-10-11 16:52:11 +0200

    Ejercicio9 changed

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -1,8 +1,26 @@
-import java.util.Scanner;
-
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         1.   Salida por pantalla                                                 *
+    *                                                                                  *
+    *         9.   Escribe un programa que pinte por pantalla alguna escena -          * 
+	*			   el campo, la habitación de una casa, un aula, etc. - o              *
+	*              algún objeto animado o inanimado - un coche, un gato, una           *
+	*			   taza de café, etc. Ten en cuenta que puedes utilizar caracteres     *
+	*              como *, +, <, #, @, etc. o incluso caracteres Unicode.              *
+	*			   ¡Échale imaginación!                                                *
+	*																		           *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 public class Ejercicio9 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		//Outputs
        	System.out.println(".---------------------------------------------------------.");
        	System.out.println(":      __                                                 :"); 
       	System.out.println(":   =='_))  __-:!:-     (Adrian Perogil Fernandez)        :"); 
@@ -10,6 +28,5 @@ public class Ejercicio9 {
       	System.out.println(":    ((_,'  .'-:!:-     (CPIPF Nuevo)                     :"); 
         System.out.println(":   ~^~~~^~~^~~~^~                                        :"); 
         System.out.println("'---------------------------------------------------------'");
-		sc.close();
 	}
 }
