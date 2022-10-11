993598401abedcb10a4b6d85e26e1ab73fb05d5e
Adrian <chopon007@gmail.com>
2022-10-11 16:46:21 +0200

    Ejercicio8 changed

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,14 +1,32 @@
-import java.util.Scanner;
-
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         1.   Salida por pantalla                                                 *
+    *                                                                                  *
+    *         7.   Igual que el programa anterior, pero esta vez la pirámide           *
+	*			   estará hueca (se debe ver únicamente el contorno hecho con *)       *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 public class Ejercicio8 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		//Variable "altura" & "asterisco"
 		int altura = 5;
 		int asterisco = 1;
+		//Variable "i"
+		//First loop for "altura"
 		for ( int i = altura ; i >= 1 ; i-- ) {
+			//Variable "espacio"
+			//Second loop for "espacio"
 			for ( int espacio = i ; espacio <= altura ; espacio++ ) {
 				System.out.print(" ");
 			}
+			//Third loop for "asterisco"
 			for ( asterisco = 1 ; asterisco <= ( i * 2 ) - 1 ; asterisco++ ) {
 				if (i == altura || (asterisco == 1 || asterisco == (2 * i) - 1)) {
 					System.out.print("*");
@@ -18,6 +36,5 @@ public class Ejercicio8 {
 			}
 			System.out.println(" ");
 		}
-		sc.close();
 	}
 }
