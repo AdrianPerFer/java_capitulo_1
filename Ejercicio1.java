f107b246728c1b682ebf23b8caeac47ba20838d6
Adrian <chopon007@gmail.com>
2022-10-11 16:15:32 +0200

    Ejercicio1 changed

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,5 +1,18 @@
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         1.   Escribe una programa que muestre tu nombre por pantalla.            *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 public class Ejercicio1 {
     public static void main(String[] args) {
+        //Output
         System.out.print("Adrián Perogil Fernández");
     }
 }
