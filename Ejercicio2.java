dbae825e96b686a78bc85fc5b4e77d566e84a9b3
Adrian <chopon007@gmail.com>
2022-10-11 16:19:48 +0200

    Ejercicio2 changed

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,7 +1,23 @@
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         1.   Salida por pantalla                                                 *
+    *                                                                                  *
+    *         2.   Modifica el programa anterior para que además se muestre tu         *
+    *              dirección y tu número de teléfono. Asegúrate de que los             *
+    *              datos se muestran en líneas separadas.                              *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 public class Ejercicio2 {
     public static void main(String[] args) {
         System.out.println("Adrián Perogil Fernández");
-        System.out.println("Calle Alozaina, 2, 2ºE");
+        System.out.println("Calle Alozaina, 3 - Estación de Cártama");
         System.out.println("Tlfo: 622274396");
     }
 }
