f19c76c013d90e49f2387ca64d26fac770f87814
Adrian <chopon007@gmail.com>
2022-10-11 16:54:51 +0200

    Ejercicio10 changed

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -1,20 +1,32 @@
-import java.util.Scanner;
-
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         1.   Salida por pantalla                                                 *
+    *                                                                                  *
+    *         10.  Mejora el ejercicio anterior añadiéndole colores.                   *                                                 *
+	*																		           *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 public class Ejercicio10 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+        //Variables "rojo", "verde", "morado", "blanco"
 		String rojo = "\033[31m";
         String verde = "\033[32m";
         String morado = "\033[35m";
         String blanco = "\033[37m";
-
-        System.out.println(morado + ".---------------------------------------------------------"+ morado + "."+ blanco);
+        //Outputs
+        System.out.println(morado + ".---------------------------------------------------------."+ blanco);
         System.out.println(morado + ":"+ rojo +"      __                                                 "+ morado + ":"+ blanco); 
         System.out.println(morado + ":"+ rojo +"   =='_))  __-:!:-     "+ verde + "(Adrian Perogil Fernandez)        "+ morado + ":"+ blanco); 
         System.out.println(morado + ":"+ rojo +"     ,:' .'  ))-:!:-   "+ verde + "(1º DAM - Programacion)           "+ morado + ":"+ blanco); 
         System.out.println(morado + ":"+ rojo +"    ((_,'  .'-:!:-     "+ verde + "(CPIPF Nuevo)                     "+ morado + ":"+ blanco); 
         System.out.println(morado + ":"+ rojo +"   ~^~~~^~~^~~~^~                                        "+ morado + ":"+ blanco); 
-        System.out.println(morado + "'---------------------------------------------------------"+ morado + "'"+ blanco);
-		sc.close();
+        System.out.println(morado + "'---------------------------------------------------------'"+ blanco);
 	}
 }
